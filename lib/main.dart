1b08bb34337519e5d4a882ba48adefaacaa0bd20
kamilriabchikov-bit <kamilriabchikov@gmail.com>
2026-02-06 22:39:08 +0300

    Первый запуск
    Задана структура папок и файлов. а также стартовое окно приложения

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FinanceApp());
+}
+
+class FinanceApp extends StatelessWidget {
+  const FinanceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Finance Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Мои финансы'),
+      ),
+      body: const Center(
+        child: Text('Главная страница'),
+      ),
+    );
+  }
+}
